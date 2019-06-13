4b7b3810c5ec78c46e76253cebee4d3659c9bbc2
rruffer <rodolforuffer@yahoo.com.br>
2019-06-13 14:58:33 -0300

    .

---
+++ src/exercicios/TesteExec5.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class TesteExec5 {
+
+	public static void main(String[] args) {
+
+		Stream<Double> stream = Stream.of(2.2, 2.8, 2.5);
+		
+		Stream<Integer> out = stream.mapToInt(Double::intValue).boxed();
+		
+
+	}
+
+}
