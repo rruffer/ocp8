9f8265988bb4ddf0db3e41aee9e5c2563e5adcd4
rruffer <rodolforuffer@yahoo.com.br>
2019-09-24 07:40:29 -0300

    .

--- src/testes/Testes.java
+++ src/testes/Testes.java
@@ -1,12 +1,15 @@
 package testes;
 
-import java.time.LocalDateTime;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.function.Supplier;
 
+/**
+ * 
+ * @author rodolfo.mindtek </br>
+ *{@link http://iteratrlearning.com/java9/2016/09/13/java9-timeouts-completablefutures.html}
+ */
 public class Testes {
 
 	public static void main(String[] args) {
@@ -16,18 +19,14 @@ public class Testes {
 		CompletableFuture<Void> timeout = CompletableFuture.supplyAsync(() -> {
 
 			while (true) {
-				System.out.println("teste...");
+				//System.out.println("teste...");
 			}
 			
 		});
 		
+		System.out.println(timeout.getNumberOfDependents());
+		
 		timeout.get(10, TimeUnit.SECONDS);
-		
-		System.out.println("teste");
-		
-		if(timeout.isCancelled()) {
-			System.out.println("acabou");
-		}
 		
 		
 		} catch (InterruptedException | ExecutionException | TimeoutException e) {
